c3c09692519eea2e3c3b1e30bc944dbc64f7e1ab
Votrungtin2001 <tin01648566861@gmail.com>
2021-04-06 14:43:45 +0700

    add Loading Screen, set Full Screen

---
+++ app/src/main/java/com/example/foodapplication/LoadingScreen.java
@@ -0,0 +1,29 @@
+package com.example.foodapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingScreen.this,MainActivity.class));
+                finish();
+            }
+        }, 5000);
+        setContentView(R.layout.activity_loading_screen);
+    }
+}
