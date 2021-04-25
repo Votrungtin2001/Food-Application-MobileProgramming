89a4e7b2eb71b636a141038aa02cb0412ff4f7cd
HungLTL <18520284@gm.uit.edu.vn>
2021-04-25 21:44:51 +0700

    Added address interface to account fragment
    - Added a full interface to the "Add Address" function in Account tab,
    with hint text and radio button support
    - Added onClickEvent to Account fragment to transistion to Address
    fragment - reference is in a code comment

--- app/src/main/java/com/example/foodapplication/AccountFragment.java
+++ app/src/main/java/com/example/foodapplication/AccountFragment.java
@@ -7,12 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AccountFragment extends Fragment {
+    Button btnVoucher, btnPayment, btnAddress, btnInvite, btnSupport, btnShop, btnPolicy, btnSettings, btnAbout, btnLogout;
+    Fragment newFragment;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        btnAddress = view.findViewById(R.id.btnAddress);
+        btnAddress.setOnClickListener(runAddressFragment);
+
+        return view;
     }
+
+    View.OnClickListener runAddressFragment = v -> {
+        newFragment = new AccountAddressFragment();
+        // source: https://stackoverflow.com/questions/21028786/how-do-i-open-a-new-fragment-from-another-fragment
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace( ((ViewGroup)getView().getParent()).getId(), newFragment, null)
+                .addToBackStack(null)
+                .commit();
+    };
 }
